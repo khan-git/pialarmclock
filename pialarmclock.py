f4ea3808133679609da52f9947736e9e65617389
khan-git <khan-git@users.noreply.github.com>
2016-04-02 23:03:40 +0200

    First class

---
+++ pialarmclock.py
@@ -0,0 +1,22 @@
+import os, sys
+import pygame
+
+class AlarmClock:
+    """Alarm clock class"""
+    
+    def __init__(self, width=320, height=240):
+        pygame.init()
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        
+    def MainLoop(self):
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                    
+if __name__ == "__main__":
+        MainWindow = AlarmClock()
+        AlarmClock.MainLoop()
+        
