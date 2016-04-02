f740adf60791897887febc829cb68fd338d0c06d
khan-git <khan-git@users.noreply.github.com>
2016-04-03 00:24:23 +0200

    Add DigitalFace

--- pialarmclock.py
+++ pialarmclock.py
@@ -1,6 +1,58 @@
-import os, sys
+import os, sys, time, datetime
 import pygame
+from Message import *
 
+
+class AnalogClock(pygame.sprite.Sprite):
+    """Clock object"""
+    
+    def __init__(self, rect, color=(0,0,255)):
+        pygame.sprite.Sprite.__init__(self)
+        self.color = color; 
+        self.rect = pygame.Rect(rect);
+        self.createImages();
+        self.image = self.baseImage
+
+    def createImages(self):
+        """Draw Logan."""
+        self.baseImage = pygame.Surface((self.rect.width, self.rect.height));
+        rect = self.baseImage.get_rect()
+
+        ## Face
+        pygame.draw.circle(self.baseImage, self.color, rect.center, rect.width/2, 1);
+
+    def drawArms(self):
+        """Draw tha actual arms"""
+        rect = self.baseImage.get_rect()
+        pygame.draw.line(self.baseImage, self.color, rect.center, (rect.width/2, 0), 1)
+        pygame.draw.line(self.baseImage, self.color, rect.center, (rect.width/2, 0), 1)
+        pygame.draw.line(self.baseImage, self.color, rect.center, (rect.width/2, 0), 1)
+        
+    def update(self):
+        """Update ticks"""
+        self.drawArms()
+
+class DigitalFace(pygame.sprite.Sprite):
+    
+    def __init__(self, rect, color=(0,0,255)):
+        pygame.sprite.Sprite.__init__(self)
+        self.color = color; 
+        self.rect = pygame.Rect(rect);
+        self.createImages();
+        self.image = self.baseImage
+
+    def createImages(self):
+        """Draw Logan."""
+        self.baseImage = pygame.Surface((self.rect.width, self.rect.height));
+            
+    def update(self):
+        """Update ticks"""
+        msgSprite = pygame.sprite.GroupSingle(
+            Message((time.strftime("%H:%M:%S", time.localtime(time.time())),), fontsize=50, align="center"));
+        msgSprite.sprite.rect.topleft = self.baseImage.get_rect().topleft;
+        msgSprite.draw(self.baseImage)
+        
+                
 class AlarmClock:
     """Alarm clock class"""
     
@@ -8,15 +60,21 @@ class AlarmClock:
         pygame.init()
         self.width = width
         self.height = height
-        self.screen = pygame.display.set_mode((self.width, self.height),pygame.FULLSCREEN)
-        
-    def MainLoop(self):
+        self.time = pygame.time.Clock();
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.clock = pygame.sprite.GroupSingle(DigitalFace(pygame.Rect((0, 0),(self.height, self.height))));
+
+    def run(self):
         while 1:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                    
+            self.clock.update()
+            self.clock.draw(self.screen)
+            pygame.display.flip()
+            self.time.tick(1)
+            
 if __name__ == "__main__":
-        MainWindow = AlarmClock()
-        MainWindow.MainLoop()
+        clock = AlarmClock()
+        clock.run()
         
